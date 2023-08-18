09ff8525d249a430a38abaadf5a2361c66948a2e
Shashivardhan <vadyala.reddy@amagi.com>
2023-08-18 11:00:15 +0530

    initial commit: plan is to extract individual elementary PES streams from TS stream first. Then do further PES decoding if necessary.

---
+++ temp.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct tspacket
+{
+    char header[4];
+    char content[184];
+};
+
+int main(int argc, char **argv) {
+    vector<string> args(argv, argv+argc);
+
+    vector<tspacket *> stream;
+
+    ifstream ifile(args[1], ios::binary);
+    ofstream ofile(args[2]);
+    
+    if (!ifile.is_open()) {
+        cerr << "Error opening " << args[0] << endl;
+        return 1;
+    }
+    if (!ofile.is_open()) {
+        cerr << "Error opening " << args[1] << endl;
+        return 1;
+    }
+    char byte;
+    int packet_bytes = 188;
+    while (ifile.read(&byte, 1)) {
+        // for (int i = 1; i >= 0; --i) {
+        //     ofile << hex << ((byte >> i*4) & 15);
+        // }
+        // ofile << " ";
+        if(packet_bytes == 188 && byte!=0x47){
+
+        }
+    }
+
+    ifile.close();
+    ofile.close();
+    return 0;
+}
